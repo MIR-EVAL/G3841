6fdb0ae27c3e0931aa46cccda7f642a612d470d1
ekaterina.valova <e.valova@mail.ru>
2023-02-01 21:12:59 +0300

    add solution for tasks 2, 4, 6, 8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача №2.
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//Просим пользователя ввести первое число
+Console.Write("Введите первое число: ");
+
+//считываем данные с консоли первое число
+string? inputNum1 = Console.ReadLine();
+
+//Просим пользователя ввести второе число
+Console.Write("Введите второе число: ");
+
+//считываем данные с консоли второе число
+string? inputNum2 = Console.ReadLine();
+
+// Проверяем, чтобы данные не были пустыми
+if (inputNum1 != null && inputNum2 != null)
+{
+  // Парсим введенное число 1
+  int number1 = int.Parse(inputNum1);
+  // Парсим введенное число 2
+  int number2 = int.Parse(inputNum2);
+
+  //Находим наибольшее число и выводим данные в консоль
+  if (number1 > number2)
+  {
+    Console.Write("Число ");
+    Console.Write(number1);
+    Console.Write(" больше числа ");
+    Console.Write(number2);
+  }
+  else
+  {
+    Console.Write("Число ");
+    Console.Write(number2);
+    Console.Write(" больше числа ");
+    Console.Write(number1);
+
+  }
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача №8.
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//Просим пользователя ввести число
+Console.Write("Введите число: ");
+
+//Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if (inputNum != null)
+{
+  //Парсим введенное число
+  int number = int.Parse(inputNum);
+
+  //Определяем выходные значения
+  string resultLine = string.Empty;
+
+  for (int i = 2; i < number; i = i + 2)
+  {
+    resultLine = resultLine + i + ",";
+  }
+
+  if (number % 2 == 0)
+  {
+    resultLine = resultLine + number;
+  }
+
+  //Выводим данные в консоль
+  Console.WriteLine(resultLine);
+}

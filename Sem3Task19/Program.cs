5827e9da2891e4908a1d5c8b09e8812858cb1cd9
ekaterina.valova <e.valova@mail.ru>
2023-02-10 00:15:31 +0300

    add solutions for tasks 19, 21, 23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача №19.
+//Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом.
+
+
+//Метод для считывания данных от пользователя
+int ReadData(string msg1)
+{
+    Console.WriteLine(msg1);
+    return int.Parse(Console.ReadLine() ?? "0 ");
+}
+// Метод для вывода результата
+void PrintData(string msg2)
+{
+    Console.WriteLine(msg2);
+}
+// Метод для проверки на палиндром
+bool PalinTest(int num)
+{
+    bool res = false;
+    int d1 = num / 10000;
+    int d2 = (num / 1000) % 10;
+    int d3 = (num % 100) / 10;
+    int d4 = num % 10;
+    res = (d1 == d4) && (d2 == d3) ? true : false;
+
+    return res;
+}
+
+// Считываем данные пользователя
+int num = ReadData("Введите пятизначное число: ");
+
+// Проверяем, является ли число пятизначным
+if (num > 9999 && num < 100000)
+{
+    // Выводим результат пользователю
+    if (PalinTest(num) == true)
+    {
+        PrintData("Это палиндром");
+    }
+    else
+    {
+        PrintData("Это не палиндром");
+    }
+}
+else
+{
+    PrintData("Число не является пятизначным! Введите корректное значение!");
+}
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,40 @@
+﻿///Задача №21.
+//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод выводит результат пользователю
+void PrintData(string msg, double res)
+{
+  Console.WriteLine(msg + res);
+}
+
+//Метод вычисляет расстояние между точками в 3D пространстве
+double CalcLen(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+  double res = 0;
+  res = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+  return res;
+}
+
+//Вводим координаты точек
+int coordX1 = ReadData("Введите координату X1");
+int coordX2 = ReadData("Введите координату X2");
+int coordY1 = ReadData("Введите координату Y1");
+int coordY2 = ReadData("Введите координату Y2");
+int coordZ1 = ReadData("Введите координату Z1");
+int coordZ2 = ReadData("Введите координату Z2");
+
+
+//Вызываем метод для вычисления расстояния между точками
+double len = CalcLen(coordX1, coordX2, coordY1, coordY2, coordZ1, coordZ2);
+
+//Выводим результат
+PrintData("Расстояние между точками: ", len);
+

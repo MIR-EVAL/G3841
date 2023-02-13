4441a91c3e091d5f4e4989ea416dc59100de9ad5
ekaterina.valova <e.valova@mail.ru>
2023-02-14 01:32:34 +0300

    add solutions for tasks 25, 27, 29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №25.
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//Метод для считывания данных от пользователя
+int ReadData(string msg1)
+{
+  Console.WriteLine(msg1);
+  return int.Parse(Console.ReadLine() ?? "0 ");
+}
+//Метод для вывода результата
+void PrintData(string msg2, int res)
+{
+  Console.WriteLine(msg2 + res);
+}
+
+//Метод для возведения числа в степень
+int Pow(int a, int b)
+{
+  int res = 1;
+  while (b > 0)
+  {
+    b = b - 1;
+    res = res * a;
+  }
+  return res;
+}
+// Считываем данные пользователя
+int numA = ReadData("Введите число A: ");
+int numB = ReadData("Введите число B: ");
+
+//Запускаем метод по возведению в степень
+int result = Pow(numA, numB);
+
+//Выводим результат пользователю
+PrintData("Число А в степени В равно ", result);
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача #27.
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Метод для считывания данных от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+//Метод для вывода результата пользователю
+void PrintData(string msg1, int msg2)
+{
+  Console.WriteLine(msg1);
+  Console.WriteLine(msg2);
+}
+
+//Метод для нахождения суммы цифр, из которых состоит число
+int SumSym(int num)
+{
+  int res = 0;
+  while (num > 0)
+  {
+    res = res + num % 10;
+    num = num / 10;
+  }
+  return res;
+}
+
+//Считываем данные пользователя
+int number = ReadData("Введите число: ");
+
+//запускаем метод нахождения суммы цифр
+int result = SumSym(number);
+
+//Выводим результат пользователю
+PrintData("Сумма цифр в данном числе равна ", result);

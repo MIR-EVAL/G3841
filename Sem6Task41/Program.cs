99cda79c6347346c2f8189409ae5917043512c2b
ekaterina.valova <e.valova@mail.ru>
2023-02-21 19:18:41 +0300

    add solution for tasks 41, 43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача №41.
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Метод cчитывает данные
+string ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine();
+}
+
+// Метод проверяет количество чисел больше 0 и выводит результат
+void CheckPositive(string[] arr)
+{
+    int count = 0;
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (int.Parse(arr[i]) > 0)
+        {
+            count++;
+        }
+    }
+   Console.WriteLine("Количество чисел больше нуля равно: " + count);
+}
+
+
+//запускаем программу
+string nums = ReadData("Введите числа через запятую");
+string[] numbers = nums.Split(new char[] { ',' });
+CheckPositive(numbers);
+// 
+
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №43.
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//Метод читает данные от пользователя
+double ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return double.Parse(Console.ReadLine() ?? "0");
+}
+
+
+//Метод выводит результат пользователю
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+
+//Проверяем, пересекаются ли прямые и находим точку их пересечения
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются!");
+}
+
+else
+
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (b2 * k1 - b1 * k2) / (k1 - k2);
+
+    PrintData("Точка пересечения прямых: (" + x + ", " + y + (')'));
+    
+}
+

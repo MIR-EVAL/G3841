8deb7f2cc17546ddc56cc8cc160c4f4264ab906a
ekaterina.valova <e.valova@mail.ru>
2023-03-02 19:16:06 +0300

    add solution for tasks 64, 66, 68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача №66.
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод выводит результат
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+//нахождение суммы с помощью рекурсии
+
+int RecSumMN(int numM, int numN)
+{
+    if (numM >= numN)
+    {
+        return numN;
+    }
+    else
+    {
+        return numM + RecSumMN(numM + 1, numN);
+    }
+}
+
+//Запускаем программу
+int numM = ReadData("Введите число M: ");
+int numN = ReadData("Введите число N: ");
+int result = numM < numN ? RecSumMN(numM, numN) : RecSumMN(numN, numM);
+PrintResult(result);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,52 @@
+﻿// //Задача №68.
+// // Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// // Даны два неотрицательных числа m и n.
+// // m = 2, n = 3 -> A(m,n) = 9
+// // m = 3, n = 2 -> A(m,n) = 29
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод выводит результат
+void PrintResult(long prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// функция Аккермана с помощью рекурсии
+int Ackerman(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    if (numM != 0 && numN == 0)
+    {
+        return Ackerman(numM - 1, 1);
+    }
+    if (numM > 0 && numN > 0)
+    {
+        return Ackerman(numM - 1, Ackerman(numM, numN - 1));
+    }
+    return Ackerman(numM, numN);
+}
+
+//Запускаем программу:
+//задаем исходные данные
+int numM = ReadData("Введите число m:");
+int numN = ReadData("Введите число n:");
+
+//проверяем, что введенные данные больше 0 и выводим результат
+if (numM > 0 && numN > 0)
+{
+    int result = Ackerman(numM, numN);
+    PrintResult(result);
+}
+else
+{
+Console.WriteLine("Введено отрицательное число!");
+}
